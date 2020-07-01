dc32ceab2689779cdf591e1ce6218b73aea2159d
Liufan666 <1663531467@qq.com>
2020-07-01 20:44:27 +0800

    提交

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,46 @@
 -- **Database level:**
 -- Display all the database
+show database;
 
 -- Enter a certain database
+use test;
 
 -- Create a database
+create database test;
 
 -- Create the database of the designated character set
+create database if not exists test default character set = 'utf8';
 
 -- Display the creation information fo the database
+show  create  database  test3;
 
 -- Revise the codes of the database
+alter database test character set utf8;
 
 -- Delete a database
+drop database test;
 
 -- **Table level**
 -- Revise table name
+alter table test1 rename test2;
 
 -- Revise the field's data type
+alter table test1 modify column id int(10);
 
 -- Revise field name
+alter table test1 change id s_id varchar(10);
 
 -- Add field
+alter table test1 add column name varchar(20);
 
 -- Delete field
+alter table test1 drop column name;
 
 -- Revise the table's storage engine
+alter table test1 ENGINE=InnoDB;
 
 -- Delete the table's foreign key restriant
+alter table test1 drop foreign key restriant_name;
 
 -- Delete a table
+drop table test1;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,16 @@
 -- Insert record
+insert into student
+(id, name, age ,sex) 
+values 
+(001, 'zhangsan', 20, 'male');
 
 -- Revise record
-
+update student
+set name='lisi'
+where id =001;
+ 
 --  Delete record
+delete from student where sex='male';
 
 -- Search record
+select * from student where id=001;
